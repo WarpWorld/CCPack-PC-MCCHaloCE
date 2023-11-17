31c26bb7a605945367e31b61689a5c0b231b92ee
Crowd Control Effect Pack Synchronization Tool <kat@warp.world>
2023-11-17 20:06:13 +0000

    Crowd Control Effect Pack Synchronization Tool - Automated Commit

--- Effects/ContinuousEffect.cs
+++ Effects/ContinuousEffect.cs
@@ -1,20 +1,21 @@
-﻿namespace CrowdControl.Games.Packs;
-
-public enum ContinuousEffect
+﻿namespace CrowdControl.Games.Packs.MCCHaloCE.Effects
 {
-    AiBreak = 4,
-    Jetpack = 5,
-    HighGravity = 6,
-    LowGravity = 7,
-    SuperJump = 8,
-    BodySnatcher = 11,
-    AwkwardMoment = 12,
-    Medusa = 13,
-    TrulyInfiniteAmmo = 14,
-    MovieBars = 19,
-    Blind = 21,
-    NoCrosshair = 22,
-    Silence = 23,
-    OneShotOneKill = 24,
-    Deathless = 25,
+    public enum ContinuousEffect
+    {
+        AiBreak = 4,
+        Jetpack = 5,
+        HighGravity = 6,
+        LowGravity = 7,
+        SuperJump = 8,
+        BodySnatcher = 11,
+        AwkwardMoment = 12,
+        Medusa = 13,
+        TrulyInfiniteAmmo = 14,
+        MovieBars = 19,
+        Blind = 21,
+        NoCrosshair = 22,
+        Silence = 23,
+        OneShotOneKill = 24,
+        Deathless = 25,
+    }
 }
