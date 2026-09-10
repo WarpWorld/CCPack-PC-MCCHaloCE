34d3fe2e03451529ac9774d2b8a7cf5a50b1cbc9
Crowd Control Effect Pack Synchronization Tool <kat@warp.world>
2026-09-10 18:35:31 +0000

    Crowd Control Effect Pack Synchronization Tool - Automated Commit

--- Utilities/InjectionManagement.cs
+++ Utilities/InjectionManagement.cs
@@ -147,12 +147,12 @@ public partial class MCCHaloCE
         IntPtr caveAddress;
         try
         {
-            caveAddress = NativeMethods.VirtualAllocEx(hndProc, (IntPtr)null, (uint)cavesize, (NativeMethods.AllocationTypeEnum)(0x1000 | 0x2000), (NativeMethods.AllocationProtectEnum)0x40);
+            caveAddress = NativeMethods.VirtualAllocEx(hndProc, IntPtr.Zero, (uint)cavesize, (NativeMethods.AllocationTypeEnum)(0x1000 | 0x2000), (NativeMethods.AllocationProtectEnum)0x40);
         }
         catch (Exception ex)
         {
             CcLog.Error("Something went wrong with the cave creation");
-            return (IntPtr)null;
+            return IntPtr.Zero;
         }
         finally
         {
